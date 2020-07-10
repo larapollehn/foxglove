fff184dfefbf24cc832e9e191704608dec1b45d2
larapollehn <lara.pollehn@googlemail.com>
2020-07-10 16:27:56 +0200

    product delete needs work

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,7 +2,13 @@ const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
 const Product = require("../models/product");
+const log = require("../utils/Logger");
 
+/**
+ * create new product based on input form data
+ * @param req
+ * @param res
+ */
 exports.create = (req, res) => {
     const form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -24,12 +30,46 @@ exports.create = (req, res) => {
     });
 };
 
+/**
+ * fetches product from db based on product id
+ * @param req
+ * @param res
+ * @param next
+ * @param id from product
+ */
 exports.productById = (req, res, next, id) => {
-    Product.findById(id).execute((error, product) => {
-        if(error || !product){
-            return res.status(400).json({ error: "Product not found" });
-        }
-        req.prouct = product;
-        next();
-    })
+    log.debug("product was requested:", id);
+    Product.findById(id)
+      .exec((error, product) => {
+          if (error || !product) {
+              return res.status(400)
+                .json({ error: "Product not found" });
+          }
+          req.product = product ;
+          next();
+      })
 };
+
+/**
+ * returns the requested product based on the product id in params
+ */
+exports.read = (req, res) => {
+    req.product.photo = undefined; // set to undefined because it is not needed for this operation and file size could be huge
+    return res.json(req.product);
+}
+
+/**
+ * remove product
+ * @param req
+ * @param res
+ */
+exports.removeProduct = (req, res) => {
+    const product = req.product;
+    log.debug("product was requested to be deleted", product.id);
+    product.remove((err, deleteProduct) => {
+        if(err){
+            return res.status(400).send("Product could not be deleted");
+        }
+        res.json({deleteProduct, message: "Product deleted successfully"});
+    })
+}
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const { create, productById } = require("../controllers/product");
+const { create, productById, read, removeProduct } = require("../controllers/product");
 const { userById } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { createProduct } = require("../validator/product");
 
+router.get("/product/:productId", read);
 router.post("/product/create/:userId", createProduct, requireSignin, isAuth, isAdmin, create);
+router.delete("/product/:productId/:userId", removeProduct);
 
 router.param("userId", userById);
 router.param("productId", productById);
