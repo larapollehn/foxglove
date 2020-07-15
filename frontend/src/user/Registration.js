83188c4ba58c9fd62f63718bb15a437985e1978d
larapollehn <lara.pollehn@googlemail.com>
2020-07-15 21:42:52 +0200

    added proxy for development in frontend package.json

--- frontend/src/user/Registration.js
+++ frontend/src/user/Registration.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import Layout from "../base/Layout";
 
 /**
  * https://app.swaggerhub.com/apis/larapollehn/buchling/1.0.0#/auth/post_signup
  */
 const Registration = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    })
+
+    // the following is the arrow version of a higher order function
+    const handleChange = targetValue => event => {
+        setValues({...values, error: false, [targetValue]: event.target.value})
+    }
 
     const registrationForm = () => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
-                <input type="text" className="form-control"/>
+                <input onChange={handleChange("name")} type="text" className="form-control"/>
             </div>
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input type="email" className="form-control"/>
+                <input onChange={handleChange("email")}  type="email" className="form-control"/>
             </div>
             <div className="form-group">
                 <label className="text-muted">Password</label>
-                <input type="password" className="form-control"/>
+                <input onChange={handleChange("password")}  type="password" className="form-control"/>
             </div>
             <button className="btn btn-primary">Register</button>
         </form>
@@ -31,6 +43,7 @@ const Registration = () => {
                 description="To start going on book adventures create a user account"
                 className="container col-md-8 offset-md-2">
                 {registrationForm()}
+                {JSON.stringify(values)}
             </Layout>
         </div>
     )
