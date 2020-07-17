7873db699a4d63402ec19114926bc6e9d04997dc
larapollehn <lara.pollehn@googlemail.com>
2020-07-17 20:00:21 +0200

    removed unused method from Home in Fe

--- frontend/src/base/Home.js
+++ frontend/src/base/Home.js
@@ -6,17 +6,6 @@ import Layout from "./Layout";
  * https://app.swaggerhub.com/apis/larapollehn/buchling/1.0.0#/product/get_products_listAll
  */
 const Home = () => {
-    const getSoldProducts = () => {
-        axios({
-            method: 'GET',
-            url: `/api/products?sortBy=${sortBy}&order=desc&limit=6`,
-        }).then((response) => {
-
-        }).catch((error) => {
-
-        })
-    }
-
     return (
         <div><Layout title="Home Page" description="MERN Stack app"/></div>
     )
