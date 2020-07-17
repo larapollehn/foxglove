5debfe14376152bddb6bff9c6d3ce450bfe7f505
larapollehn <lara.pollehn@googlemail.com>
2020-07-17 19:37:39 +0200

    fixed linting in backend

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -10,26 +10,26 @@ const log = require("../utils/Logger");
  * @param res
  */
 exports.create = (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400)
-        .json({ error: err.message });
-    }
-    const product = new Product(fields);
-    if (files.photo) {
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
-    }
-    product.save((error, result) => {
-      if (error) {
-        return res.status(400)
-          .json({ error: error.message });
-      }
-      return res.json({ result });
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400)
+                .json({error: err.message});
+        }
+        const product = new Product(fields);
+        if (files.photo) {
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+        product.save((error, result) => {
+            if (error) {
+                return res.status(400)
+                    .json({error: error.message});
+            }
+            return res.json({result});
+        });
     });
-  });
 };
 
 /**
@@ -40,24 +40,24 @@ exports.create = (req, res) => {
  * @param id from product
  */
 exports.productById = (req, res, next, id) => {
-  log.debug("product was requested:", id);
-  Product.findById(id)
-    .exec((error, product) => {
-      if (error || !product) {
-        return res.status(400)
-          .json({ error: "Product not found" });
-      }
-      req.product = product;
-      next();
-    });
+    log.debug("product was requested:", id);
+    Product.findById(id)
+        .exec((error, product) => {
+            if (error || !product) {
+                return res.status(400)
+                    .json({error: "Product not found"});
+            }
+            req.product = product;
+            next();
+        });
 };
 
 /**
  * returns the requested product based on the product id in params
  */
 exports.read = (req, res) => {
-  req.product.photo = undefined; // set to undefined because it is not needed for this operation and file size could be huge
-  return res.json(req.product);
+    req.product.photo = undefined; // set to undefined because it is not needed for this operation and file size could be huge
+    return res.json(req.product);
 };
 
 /**
@@ -66,18 +66,18 @@ exports.read = (req, res) => {
  * @param res
  */
 exports.removeProduct = (req, res) => {
-  const { product } = req;
-  log.debug("product was requested to be deleted", product.id);
-  product.remove((err, deleteProduct) => {
-    if (err) {
-      return res.status(400)
-        .send("Product could not be deleted");
-    }
-    res.json({
-      deleteProduct,
-      message: "Product deleted successfully",
+    const {product} = req;
+    log.debug("product was requested to be deleted", product.id);
+    product.remove((err, deleteProduct) => {
+        if (err) {
+            return res.status(400)
+                .send("Product could not be deleted");
+        }
+        res.json({
+            deleteProduct,
+            message: "Product deleted successfully",
+        });
     });
-  });
 };
 
 /**
@@ -86,27 +86,27 @@ exports.removeProduct = (req, res) => {
  * @param res
  */
 exports.updateProduct = (req, res) => {
-  const form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return res.status(400)
-        .json({ error: err.message });
-    }
-    let { product } = req;
-    product = _.extend(product, fields);
-    if (files.photo) {
-      product.photo.data = fs.readFileSync(files.photo.path);
-      product.photo.contentType = files.photo.type;
-    }
-    product.save((error, result) => {
-      if (error) {
-        return res.status(400)
-          .json({ error: error.message });
-      }
-      return res.json({ result });
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400)
+                .json({error: err.message});
+        }
+        let {product} = req;
+        product = _.extend(product, fields);
+        if (files.photo) {
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+        product.save((error, result) => {
+            if (error) {
+                return res.status(400)
+                    .json({error: error.message});
+            }
+            return res.json({result});
+        });
     });
-  });
 };
 
 /**
@@ -116,35 +116,35 @@ exports.updateProduct = (req, res) => {
  * @param res
  */
 exports.getRelatedProducts = (req, res) => {
-  log.debug("Related proucts were requested");
-  const limit = req.query.limit ? parseInt(req.query.limit) : 3;
-  Product.find({
-    _id: { $ne: req.product },
-    category: req.product.category,
-  })
-    .limit(limit)
-    .populate("category", "_id name")
-    .exec((err, products) => {
-      if (err) {
-        return res.status(400)
-          .json({ error: "Products found" });
-      }
-      log.debug("Related products were found, amount:", products.length);
-      return res.json({ products });
-    });
+    log.debug("Related proucts were requested");
+    const limit = req.query.limit ? parseInt(req.query.limit) : 3;
+    Product.find({
+        _id: {$ne: req.product},
+        category: req.product.category,
+    })
+        .limit(limit)
+        .populate("category", "_id name")
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400)
+                    .json({error: "Products found"});
+            }
+            log.debug("Related products were found, amount:", products.length);
+            return res.json({products});
+        });
 };
 
 exports.listProductCategories = (req, res) => {
-  log.debug("List of all product categories was requested");
-  Product.distinct("category", {}, (err, productCategories) => {
-    if (err) {
-      return res.status(400)
-        .json({
-          error: "No product catgories found",
-        });
-    }
-    res.json({ productCategories });
-  });
+    log.debug("List of all product categories was requested");
+    Product.distinct("category", {}, (err, productCategories) => {
+        if (err) {
+            return res.status(400)
+                .json({
+                    error: "No product catgories found",
+                });
+        }
+        res.json({productCategories});
+    });
 };
 
 /**
@@ -154,46 +154,46 @@ exports.listProductCategories = (req, res) => {
  * @param res
  */
 exports.listBySearch = (req, res) => {
-  const order = req.body.order ? req.body.order : "desc";
-  const sortBy = req.body.sortBy ? req.body.sortBy : "_id";
-  const limit = req.body.limit ? parseInt(req.body.limit) : 100;
-  const skip = parseInt(req.body.skip);
-  const findArgs = {};
-  log.debug("Product was searched by user:", order, sortBy, limit);
+    const order = req.body.order ? req.body.order : "desc";
+    const sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    const limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    const skip = parseInt(req.body.skip);
+    const findArgs = {};
+    log.debug("Product was searched by user:", order, sortBy, limit);
 
-  for (const key in req.body.filters) {
-    if (req.body.filters[key].length > 0) {
-      if (key === "price") {
-        // gte -  greater than price [0-10]
-        // lte - less than
-        findArgs[key] = {
-          $gte: req.body.filters[key][0],
-          $lte: req.body.filters[key][1],
-        };
-      } else {
-        findArgs[key] = req.body.filters[key];
-      }
+    for (const key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1],
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
     }
-  }
 
-  log.debug("Products in range should have the args:", findArgs);
-  Product.find(findArgs)
-    .select("-photo")
-    .populate("category")
-    .sort([[sortBy, order]])
-    .skip(skip)
-    .limit(limit)
-    .exec((err, data) => {
-      if (err) {
-        return res.status(400).json({
-          error: "Products not found",
+    log.debug("Products in range should have the args:", findArgs);
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([[sortBy, order]])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found",
+                });
+            }
+            res.json({
+                size: data.length,
+                data,
+            });
         });
-      }
-      res.json({
-        size: data.length,
-        data,
-      });
-    });
 };
 
 /**
@@ -204,12 +204,12 @@ exports.listBySearch = (req, res) => {
  * @returns {boolean | void}
  */
 exports.getProductPhoto = (req, res, next) => {
-  log.debug("Product photo is requested");
-  if (req.product.photo.data) {
-    res.set("Content-Type", req.product.photo.contentType);
-    return res.send(req.product.photo.data);
-  }
-  next();
+    log.debug("Product photo is requested");
+    if (req.product.photo.data) {
+        res.set("Content-Type", req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
 };
 
 /**
@@ -218,15 +218,15 @@ exports.getProductPhoto = (req, res, next) => {
  * @param res
  */
 exports.listAllProducts = (req, res) => {
-  log.debug("List of products was requested");
-  Product.find({sold: {$gt: 0}})
-      .limit(6)
-      .exec((err, products) => {
-        if (err) {
-          return res.status(400)
-              .json({ error: "No Products found" });
-        }
-        log.debug("Sold products were found");
-        return res.json({ products });
-      });
-}
+    log.debug("List of products was requested");
+    Product.find({sold: {$gt: 0}})
+        .limit(6)
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400)
+                    .json({error: "No Products found"});
+            }
+            log.debug("Sold products were found");
+            return res.json({products});
+        });
+};
--- frontend/src/base/Home.js
+++ frontend/src/base/Home.js
@@ -2,8 +2,10 @@ import React from "react";
 
 import Layout from "./Layout";
 
+/**
+ * https://app.swaggerhub.com/apis/larapollehn/buchling/1.0.0#/product/get_products_listAll
+ */
 const Home = () => {
-
     const getSoldProducts = () => {
         axios({
             method: 'GET',
